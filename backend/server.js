257004c61acdcd89ecaaf5bd3f3b13a07cfca104
Delta-WQ <pente787@gmail.com>
2026-08-15 16:49:41 -0300

    Update static file serving and index route

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// servir todos os arquivos da pasta backend
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '..')));
 
-// rota principal: abre index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
